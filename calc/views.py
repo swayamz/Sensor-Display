254da745906734e8fc306cccd9234cffd89c59c1
swayamz <swayamsaraiya@gmail.com>
2019-06-28 15:27:18 -0700

    Heatmap V1

--- calc/views.py
+++ calc/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name':'World'})
+    return render(request, 'home.html', {'data1':'World'})
