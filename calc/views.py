b548e3cc70c953370a2fea0e2a2722d17e722edb
swayamz <swayamsaraiya@gmail.com>
2019-07-23 23:02:31 -0700

    Front End Overhaul

--- calc/views.py
+++ calc/views.py
@@ -8,6 +8,7 @@ from django.core import serializers
 # Create your views here.
 
 def home(request):
+    getLatestTime = FarmData.objects.values_list('time', flat=True).latest('id')
     #myData = FarmData.objects.all().filter(time='19 Jul 2019 11:19:26')
    ##myData_time = list(map(str, list(FarmData.objects.values_list('time', flat=True).filter(time='19 Jul 2019 11:19:26'))))
    # myData_group = list(map(str, list(FarmData.objects.values_list('x', flat=True).filter(time='19 Jul 2019 11:19:26'))))
@@ -29,10 +30,11 @@ def home(request):
     # print(table)
     # print(myData_time, myData_group, myData_value, myData_variable)
     #return render(request, 'home.html', {'myData_group': myData_group, 'myData_value': myData_value, 'myData_variable': myData_variable, 'matix' : matrix})
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'latestTime' : getLatestTime})
 
 def getJSONdata(request):
     getLatestTime = FarmData.objects.values_list('time', flat=True).latest('id')
     myData = FarmData.objects.all().filter(time=getLatestTime)
     print(serializers.serialize('json', myData))
     return JsonResponse(serializers.serialize('json', myData), safe=False)
+
